f3fc748d795d1b81b9cfa05385411e143145e3d2
codetestingforcdinc <89401300+codetestingforcdinc@users.noreply.github.com>
2022-10-31 17:59:49 +0530

    commiting

---
+++ app/config.php
@@ -0,0 +1,11 @@
+<?php
+//Config Database 
+define('DB_HOST', 'localhost');        //don't change it
+define('DB_USER', 'smmprim1_32');      //enter your database username
+define('DB_PASS', 'smmprim1_32');      //enter your database password
+define('DB_NAME', 'smmprim1_32');      //enter you database name
+define('TIMEZONE', 'Africa/Abidjan');  //don't change
+define('ENCRYPTION_KEY', 'b574f0920c60bee93e628378455dfbaa'); //don't change
+
+//This Script is Distributed by https://downloaddigital.store for free
+//For installation contact https://wa.link/jhhnpg
